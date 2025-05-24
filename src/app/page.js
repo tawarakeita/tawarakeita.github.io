52bd35d215c44ae4c8dfe0b07d4f3121215714f3
tawarakeita <keita_tawara@icloud.com>
2025-05-24 22:06:00 +0900

    不要なimportを削除

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,3 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
 export default function Home() {
   return (
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
