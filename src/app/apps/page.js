74d16a2d12d17cdd8dce0eb9ce9b8f31a3e6c4d9
tawarakeita <keita_tawara@icloud.com>
2025-07-02 15:14:47 +0900

    アプリページの変更

--- src/app/apps/page.js
+++ src/app/apps/page.js
@@ -3,13 +3,16 @@ import Title from "../../components/Title";
 
 export default function Page() {
   return (
-      <main>
-        <Title mainText = "制作アプリ" subText = "Apps" />
-        <div>
-          <Link href="/apps/shareyourlife">
-            <span>shareYourLIFE</span>
-          </Link>
-        </div>
+      <main className='p-4'>
+        <Title mainText = "Kei Apps" subText = "Kei Apps" />
+          <div className='flex'>
+            <Link href="/apps/shareyourlife" className='h-[220px] w-[220px] m-3 bg-[#CD853F] flex items-center justify-center rounded-lg'>
+              <span className='text-3xl text-white'>shareYourLIFE</span>
+            </Link>
+            <Link href="/apps/mottainai" className='h-[220px] w-[220px] m-3 bg-[#485fc7] flex items-center justify-center rounded-lg'>
+              <span className='text-3xl text-white'>もったいない！</span>
+            </Link>
+          </div>
       </main>
   );
 }
