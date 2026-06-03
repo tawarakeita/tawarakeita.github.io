d8cc65b9714bc49793b18b197983821bb1a49106
tawarakeita <keita_tawara@icloud.com>
2026-06-03 22:01:22 +0900

    トップページのスマホ用スタイルを作成

--- src/app/page.js
+++ src/app/page.js
@@ -5,20 +5,20 @@ import { faGithub, faInstagram, faXTwitter } from '@fortawesome/free-brands-svg-
 export default function Home() {
   return (
       <main className="flex flex-col gap-8 row-start-2 items-center min-h-screen justify-center">
-        <div className="flex flex-col gap-8 items-center">
+        <div className="flex flex-col gap-8 items-center ml-5">
           <h1 className="text-8xl font-bold">Tawara Keita</h1>
-          <div>
+          <div className="mr-auto sm:m-auto flex gap-5 sm:gap-6">
             <a className="" href="https://x.com/k_tawara0102">
-              <FontAwesomeIcon className='m-[15px]' size="2x" icon={faXTwitter} />
+              <FontAwesomeIcon size="2x" icon={faXTwitter} />
             </a>
             <a className="" href="https://www.instagram.com/ecopay_tawara">
-              <FontAwesomeIcon className='m-[15px]' size="2x" icon={faInstagram} />
+              <FontAwesomeIcon size="2x" icon={faInstagram} />
             </a>
             <a className="" href="https://github.com/tawarakeita">
-              <FontAwesomeIcon className='m-[15px]' size="2x" icon={faGithub} />
+              <FontAwesomeIcon size="2x" icon={faGithub} />
             </a>
           </div>
-          <div className="flex gap-4 flex-col items-center text-2xl">
+          <div className="flex gap-4 flex-col items-start text-2xl mr-auto sm:m-auto sm:items-center">
             <Link href="/profile">
               <span>Profile</span>
             </Link>
