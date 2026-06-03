45ab84ccd0eda6805a28854831525935adfd30aa
tawarakeita <keita_tawara@icloud.com>
2026-06-03 23:02:48 +0900

    お問い合わせページのスマホ用スタイルを作成

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,9 +1,13 @@
 export default function Page() {
   return (
-    <iframe
-      src="https://docs.google.com/forms/d/e/1FAIpQLScY7yA9YTvtbiXNoUnphjiWATnTnMPCEPdEtq2Wl1aR50RZBw/viewform?embedded=true"
-      title="お問い合わせフォーム"
-      className="w-[70dvw] h-[80dvh] mt-28 m-auto overflow-hidden border-2 border-gray-600 rounded-xl shadow-xl"
-    >読み込み中</iframe>
+    <main className='sm:min-h-screen flex sm:items-center justify-center mt-20 sm:mt-0 p-6'>
+      <div className="w-full h-[75vh] max-w-3xl border-2 border-gray-600 rounded-xl shadow-xl">
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLScY7yA9YTvtbiXNoUnphjiWATnTnMPCEPdEtq2Wl1aR50RZBw/viewform?embedded=true"
+          title="お問い合わせフォーム"
+          className="w-full h-full"
+        >読み込み中</iframe>
+      </div>
+    </main>
   );
 }
